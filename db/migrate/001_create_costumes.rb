f7692c0320b6882d4ba0844d9a6ff52cf96ebb7a
Frank McCallion <fkmccallion@gmail.com>
2018-03-31 16:39:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :name
       t.float :price
       t.string :size
-      t.string :url
+      t.string :image_url
     end
   end
 
