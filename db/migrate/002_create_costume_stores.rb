e77a44166a626aa26763b8675b0de281f68b2e60
Frank McCallion <fkmccallion@gmail.com>
2018-03-31 16:22:25 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -11,5 +11,5 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.datetime :closing
     end
   end
-  
+
 end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :friend
+      t.datetime :opening
+      t.datetime :closing
+      t.string :description
+    end
+  end
+
+end
