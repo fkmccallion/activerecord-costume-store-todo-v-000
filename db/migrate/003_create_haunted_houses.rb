87756113bf4fa1221d2ac92fa73689d7d5d7fd83
Frank McCallion <fkmccallion@gmail.com>
2018-03-31 16:49:07 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :friend
+      t.boolean :family_friendly
       t.datetime :opening
       t.datetime :closing
       t.string :description
