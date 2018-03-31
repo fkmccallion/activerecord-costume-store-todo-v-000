6c95668b5d20892ef0b98a0a833be2c4489e8414
Frank McCallion <fkmccallion@gmail.com>
2018-03-31 16:45:38 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :inventory
+      t.integer :costume_inventory
       t.integer :employees
       t.boolean :open
       t.datetime :opening
